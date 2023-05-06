8b341833df9ac70f80947df06dcec016faf3c887
Joseph <joeyk710@outlook.com>
2023-05-06 17:39:24 -0400

    add JSDoc info

--- src/misc/util.ts
+++ src/misc/util.ts
@@ -2,6 +2,9 @@ import { PermissionResolvable, PermissionsString, PermissionsBitField } from "di
 
 /** 
  * @function {missingPerms} This is the function for missing permissions.
+ * @param {PermissionResolvable} memberPerms The member permissions.
+ * @param {PermissionResolvable} requiredPerms The required permissions.
+ * @returns {PermissionsString[]} The missing permissions.
 */
 export function missingPerms(memberPerms: PermissionResolvable, requiredPerms: PermissionResolvable): PermissionsString[] {
     return new PermissionsBitField(memberPerms).missing(new PermissionsBitField(requiredPerms));
