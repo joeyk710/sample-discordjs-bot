71530f40b251248048b64faf417e382bb80a8d77
Joseph <joeyk710@outlook.com>
2023-07-16 15:38:18 -0400

    updated typings, imports, etc.

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,54 +1,65 @@
-import { RESTPostAPIApplicationCommandsJSONBody, REST, Routes } from 'discord.js';
+import {
+	REST,
+	type RESTPostAPIApplicationCommandsJSONBody,
+	type RESTPostAPIApplicationGuildCommandsJSONBody,
+	type RESTPutAPIApplicationCommandsJSONBody,
+	type RESTPutAPIApplicationGuildCommandsJSONBody,
+	Routes,
+} from 'discord.js';
+import { readdirSync } from 'node:fs';
+import { fileURLToPath, URL } from 'node:url';
+import { join } from 'node:path';
+
 import { dynamicImport } from './misc/util.js';
-import { CommandClass } from './structures/command.js';
+
+import type { Command } from './structures/command.js';
 import 'dotenv/config';
 
-import fs from 'node:fs';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-
-const commands: RESTPostAPIApplicationCommandsJSONBody[] = [];
+const commands: RESTPostAPIApplicationCommandsJSONBody[] | RESTPostAPIApplicationGuildCommandsJSONBody[] = [];
 
 const commandFolderPath = fileURLToPath(new URL('commands', import.meta.url));
-const commandFolders = fs.readdirSync(commandFolderPath);
+const commandFolders = readdirSync(commandFolderPath);
 
-// Grab the output of each command's data for deployment
+// Grab the output of each command for deployment
 for (const category of commandFolders) {
-	const commandPath = path.join(commandFolderPath, category)
-	const commandFiles = fs.readdirSync(commandPath).filter(file => file.endsWith('.js'));
+	const commandPath = join(commandFolderPath, category)
+	const commandFiles = readdirSync(commandPath).filter(file => file.endsWith('.js'));
 	for (const fileName of commandFiles) {
-		const filePath = path.join(commandPath, fileName);
-		const command = await dynamicImport(filePath) as CommandClass;
+		const filePath = join(commandPath, fileName);
 
-		commands.push(command.data);
+		const command: Command = await dynamicImport(filePath);
+		if ('data' in command && 'execute' in command) {
+			commands.push(command.data);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
 	};
 };
-
 // Construct and prepare an instance of the REST module
-const rest = new REST().setToken(process.env.TOKEN);
+const rest = new REST().setToken(process.env.DISCORD_TOKEN);
 
 // and deploy your commands!
 (async () => {
 	try {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-		let data: string | any[];
+		let data: RESTPutAPIApplicationCommandsJSONBody[] | RESTPutAPIApplicationGuildCommandsJSONBody[] = [];
 
-		// The put method is used to fully refresh all commands in the guild with the current set
 		if (process.env.GUILD_ID) {
+			// The put method is used to fully refresh all commands in a guild with the current set
 			data = await rest.put(
 				Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
 				{ body: commands },
-			) as CommandClass['data'][];
+			) as RESTPutAPIApplicationGuildCommandsJSONBody[];
 		} else {
 			// The put method is used to fully refresh all commands in all guilds with the current set
 			data = await rest.put(
 				Routes.applicationCommands(process.env.CLIENT_ID),
 				{ body: commands },
-			) as CommandClass['data'][];
+			) as RESTPutAPIApplicationCommandsJSONBody[];
 		};
 
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+		console.log(`Successfully reloaded ${data.length} application (/) commands ${process.env.GUILD_ID ? `in guild ${process.env.GUILD_ID}` : ''}.`);
 	} catch (error) {
 		// And of course, make sure you catch and log any errors!
 		console.error(error);
