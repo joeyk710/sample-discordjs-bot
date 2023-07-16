c58548d4988f1d5c948cd7f8996ae98b037fb0e2
Joseph <joeyk710@outlook.com>
2023-07-16 15:41:51 -0400

    new event type

--- src/structures/event.ts
+++ src/structures/event.ts
@@ -1,19 +1,22 @@
 import type { ClientEvents } from 'discord.js';
 
-interface EventOptions<Key extends keyof ClientEvents> {
+/**
+ * Defines the structure of an event.
+ */
+export type Event<Key extends keyof ClientEvents = keyof ClientEvents> = {
+    /**
+     * The name of the event to listen to
+    */
     name: Key;
+    /**
+     * Whether or not the event should only be listened to once
+    *
+    * @defaultValue false
+    */
     once?: boolean;
-    execute: (...args: ClientEvents[Key]) => Promise<any> | any;
-};
-
-export class EventClass<Key extends keyof ClientEvents> implements EventOptions<Key> {
-    name: EventOptions<Key>['name'];
-    once?: EventOptions<Key>['once'];
-    execute: EventOptions<Key>['execute'];
-
-    constructor(options: EventOptions<Key>) {
-        this.name = options.name;
-        this.once = options.once;
-        this.execute = options.execute;
-    };
+    /**
+     * The function to execute when the event is emitted.
+     * @param args - The parameters of the event
+     */
+    execute(...args: ClientEvents[Key]): Promise<void> | void;
 };
