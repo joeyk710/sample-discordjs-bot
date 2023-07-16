92a63fac42587f55b3a2ced4d9d7e606657031f5
Joseph <joeyk710@outlook.com>
2023-07-16 15:40:19 -0400

    prevents reply to get proper latency response

--- src/commands/general/ping.ts
+++ src/commands/general/ping.ts
@@ -1,7 +1,8 @@
-import { ChatInputCommandInteraction, inlineCode } from 'discord.js';
-import { CommandClass } from '../../structures/command.js';
+import { type ChatInputCommandInteraction, inlineCode, RESTJSONErrorCodes, time, TimestampStyles } from 'discord.js';
 
-export default new CommandClass({
+import type { Command } from '../../structures/command.js';
+
+export default {
     data: {
         name: 'ping',
         description: 'Pong!',
@@ -12,18 +13,36 @@ export default new CommandClass({
         category: 'General',
         cooldown: 5,
         visible: true,
-        guildOnly: false,
+        guildOnly: false
     },
     async execute(interaction: ChatInputCommandInteraction<'cached'>) {
+        const currentTime = 1000 * 75 - interaction.client.uptime;
+        const botReadyTimestamp = Math.round((Date.now() + currentTime) / 1000);
+
+        // This is to prevent from using the command so the client has a chance to output a proper latency report
+        if (interaction.client.uptime < 1000 * 75) {
+            await interaction.reply({
+                content: `The bot is still starting up. Run this command again ${time(botReadyTimestamp, TimestampStyles.RelativeTime)} to see statistical information.`,
+                ephemeral: true
+            });
+            return;
+        };
+
         const msg = await interaction.reply({
-            content: 'Pinging...',
+            content: '🏓 Pinging...',
             fetchReply: true
         });
-        setTimeout(() => {
+
+        setTimeout(async () => {
             const ping = msg.createdTimestamp - interaction.createdTimestamp;
-            interaction.editReply({
-                content: `Pong! Latency is ${inlineCode(`${ping}ms`)}. \nAPI Latency is ${inlineCode(`${interaction.client.ws.ping}ms`)}`
+            await interaction.editReply({
+                content: `Pong 🏓! \nRoundtrip Latency is ${inlineCode(`${ping}ms`)}. \nWebsocket Heartbeat is ${inlineCode(`${interaction.client.ws.ping}ms`)}`
+            }).catch((err) => {
+                if (err.code === RESTJSONErrorCodes.UnknownMessage) {
+                    console.error(`Failed to edit interaction: ${err.message}`);
+                }
             });
-        }, 3000);
-    },
-})
+            return;
+        }, 3_000);
+    }
+} satisfies Command;
