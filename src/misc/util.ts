a167b03bbb4817ad1e055ac0a1db32ac8ec2384b
Danial Raza <danialrazafb@gmail.com>
2023-07-26 20:32:28 +0200

    refactor: extract file loading into a reusable function (#25)
    * refactor: extract file loading into a reusable function
    
    * refactor: minor tweaks
    
    * style: format files
    
    ---------
    
    Co-authored-by: Joe <joeyk710@outlook.com>

--- src/misc/util.ts
+++ src/misc/util.ts
@@ -1,5 +1,7 @@
 import { type PermissionResolvable, type PermissionsString, PermissionsBitField, type APIEmbed, type Message } from 'discord.js';
 import { pathToFileURL } from 'node:url';
+import { type PathLike, readdirSync } from 'node:fs';
+import { join } from 'node:path';
 
 /**
  * This function gets the default export from a file.
@@ -9,6 +11,32 @@ export async function dynamicImport(path: string): Promise<any> {
     const module = await import(pathToFileURL(path).toString());
     return module?.default;
 };
+
+/**
+ * Loads all the structures from the provided directory path.
+ * @param {PathLike} path - The directory path to load the structures from
+ * @param {[string, string]} props - The properties to check if the structure is valid
+ */
+export async function loadStructures(path: PathLike, props: [string, string]) {
+    const fileData = [];
+
+    const folders = readdirSync(path);
+
+    for (const folder of folders) {
+        const filesPath = join(path.toString(), folder);
+        const files = readdirSync(filesPath).filter(file => file.endsWith('.js'));
+
+        for (const file of files) {
+            const filePath = join(filesPath, file);
+            const data = await dynamicImport(filePath);
+
+            if (props[0] in data && props[1] in data) fileData.push(data);
+            else console.warn(`\u001b[33m The command at ${filePath} is missing a required ${props[0]} or ${props[1]} property.`);
+        }
+    }
+
+    return fileData;
+}
 
 /** 
  * Shows the missing permissions.
@@ -28,7 +56,7 @@ export function ellipsis(text: string, total: number): string {
     if (text.length <= total) {
         return text;
     }
-  const keep = total - 3;
+    const keep = total - 3;
     if (keep < 1) return text.slice(0, total);
     return `${text.slice(0, keep)}...`;
 };
