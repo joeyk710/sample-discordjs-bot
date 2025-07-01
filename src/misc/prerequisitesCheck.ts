6bbf0cd8174d24deea54f237fd541dc612a0373f
Noah <69383725+peakh@users.noreply.github.com>
2025-06-30 23:34:42 -0400

    Consistent formatting, reduce unnecessary backtick usage

--- src/misc/prerequisitesCheck.ts
+++ src/misc/prerequisitesCheck.ts
@@ -1,41 +1,41 @@
-import { createInterface } from "node:readline";
-import { existsSync, readFileSync, writeFileSync } from "node:fs";
 import chalk from "chalk";
-import { Snowflake } from "discord.js";
+import type { Snowflake } from "discord.js";
+import { existsSync, readFileSync, writeFileSync } from "node:fs";
+import { createInterface } from "node:readline";
 
 // Create an interface for reading input from the command line
 const rl = createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
 // Error messages for invalid inputs
 const tokenError = [
-    `\n${chalk.redBright("The Application Token provided is not valid.")}`,
-    `Please make sure you copied it correctly from the ${chalk.bold.whiteBright("Discord Developer Portal > Applications > Bot > Token.")}`,
-    `If you're not sure where that is, check this link - ${chalk.bold.cyanBright("https://discord.com/developers/applications")}\n`
+  `\n${chalk.redBright("The Application Token provided is not valid.")}`,
+  `Please make sure you copied it correctly from the ${chalk.bold.whiteBright("Discord Developer Portal > Applications > Bot > Token.")}`,
+  `If you're not sure where that is, check this link - ${chalk.bold.cyanBright("https://discord.com/developers/applications")}\n`,
 ];
 const appIdError = [
-    `\n${chalk.redBright("The Application ID provided is not valid.")}`,
-    `Please make sure you copied it correctly from the ${chalk.bold.whiteBright(
-        "Discord Developer Portal > Applications > General Information > Application ID."
-    )}`,
-    `If you're not sure where that is, check this link - ${chalk.bold.cyanBright("https://discord.com/developers/applications")}\n`
+  `\n${chalk.redBright("The Application ID provided is not valid.")}`,
+  `Please make sure you copied it correctly from the ${chalk.bold.whiteBright(
+    "Discord Developer Portal > Applications > General Information > Application ID.",
+  )}`,
+  `If you're not sure where that is, check this link - ${chalk.bold.cyanBright("https://discord.com/developers/applications")}\n`,
 ];
 const guildIdError = [
-    `\n${chalk.redBright("The Guild ID provided is not valid.")}`,
-    `Please make sure you copied it correctly from the server you want commands to be registered to.`,
-    `If you're not sure how to get the Guild ID, check this link - ${chalk.bold.cyanBright(
-        "https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-"
-    )}\n`
+  `\n${chalk.redBright("The Guild ID provided is not valid.")}`,
+  "Please make sure you copied it correctly from the server you want commands to be registered to.",
+  `If you're not sure how to get the Guild ID, check this link - ${chalk.bold.cyanBright(
+    "https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-",
+  )}\n`,
 ];
 
 // Display environment variables and their current status
 const envVars = [
-    chalk.underline.greenBright(`Environment variables:`),
-    `${chalk.whiteBright(`APPLICATION_TOKEN =`)} ${process.env.APPLICATION_TOKEN?.replace(process.env.APPLICATION_TOKEN, "( ͡° ͜ʖ ͡°)") || chalk.yellow("Not set")}`,
-    `${chalk.whiteBright(`APPLICATION_ID =`)} ${process.env.APPLICATION_ID || chalk.yellow("Not set")}`,
-    `${chalk.whiteBright(`GUILD_ID =`)} ${process.env.GUILD_ID || chalk.yellow("Not set")}\n`
+  chalk.underline.greenBright("Environment variables:"),
+  `${chalk.whiteBright(`APPLICATION_TOKEN =`)} ${process.env.APPLICATION_TOKEN?.replace(process.env.APPLICATION_TOKEN, "( ͡° ͜ʖ ͡°)") || chalk.yellow("Not set")}`,
+  `${chalk.whiteBright(`APPLICATION_ID =`)} ${process.env.APPLICATION_ID || chalk.yellow("Not set")}`,
+  `${chalk.whiteBright(`GUILD_ID =`)} ${process.env.GUILD_ID || chalk.yellow("Not set")}\n`,
 ];
 
 /**
@@ -44,7 +44,7 @@ const envVars = [
  * @returns True if the token is valid, false otherwise.
  */
 function tokenCheck(token: string): boolean {
-    return /(?<snowflake>[\w-]{23,26})\.(?<timestamp>[\w-]{6})\.(?<hmac>[\w-]{27,38})/.test(token);
+  return /(?<snowflake>[\w-]{23,26})\.(?<timestamp>[\w-]{6})\.(?<hmac>[\w-]{27,38})/.test(token);
 }
 
 /**
@@ -52,8 +52,8 @@ function tokenCheck(token: string): boolean {
  * @param param - The parameter to validate.
  * @returns True if the parameter is a valid Snowflake ID, false otherwise.
  */
-function isSnowflake(param: Snowflake | string): param is Snowflake {
-    return /^(?:<@!?)?(\d{17,19})>?$/.test(param);
+function isSnowflake(param: Snowflake): param is Snowflake {
+  return /^(?:<@!?)?(\d{17,19})>?$/.test(param);
 }
 
 /**
@@ -62,15 +62,15 @@ function isSnowflake(param: Snowflake | string): param is Snowflake {
  * @returns A promise that resolves with the user's response.
  */
 async function promptUser(question: string): Promise<string> {
-    return new Promise((resolve, reject) => {
-        rl.question(question, (answer) => {
-            if (answer) {
-                resolve(answer);
-            } else {
-                reject(new Error("No answer provided"));
-            }
-        });
+  return new Promise((resolve, reject) => {
+    rl.question(question, answer => {
+      if (answer) {
+        resolve(answer);
+      } else {
+        reject(new Error("No answer provided"));
+      }
     });
+  });
 }
 
 /**
@@ -80,25 +80,32 @@ async function promptUser(question: string): Promise<string> {
  * @param validationFn - An optional validation function to validate the variable's value.
  * @param optional - Whether the environment variable is optional.
  */
-async function checkEnvVariable(variable: string, errorMessage: string, validationFn?: (value: string) => boolean, optional = false): Promise<void> {
-    if (!process.env[variable]) {
-        if (optional) {
-            console.log(chalk.yellowBright(`⚠ ${variable} is not set. Skipping...`));
-            return;
-        }
-        try {
-            const value = await promptUser(`${chalk.redBright(`No ${variable} was provided.`)} \nEnter your ${variable} here to continue operation => `);
-            if (!value || (validationFn && !validationFn(value))) {
-                console.error(errorMessage);
-                throw new Error(errorMessage);
-            }
-            console.log(chalk.greenBright(`✅ ${variable} added.`), chalk.whiteBright("Continuing operation."));
-            overwriteLine(".env", variable, value);
-        } catch (error) {
-            console.error(chalk.redBright(`Failed to set ${variable}: ${error.message}`));
-            throw error;
-        }
+async function checkEnvVariable(
+  variable: string,
+  errorMessage: string,
+  validationFn?: (value: string) => boolean,
+  optional = false,
+): Promise<void> {
+  if (!process.env[variable]) {
+    if (optional) {
+      console.log(chalk.yellowBright(`⚠ ${variable} is not set. Skipping...`));
+      return;
     }
+    try {
+      const value = await promptUser(
+        `${chalk.redBright(`No ${variable} was provided.`)}\nEnter your ${variable} here to continue operation => `,
+      );
+      if (!value || (validationFn && !validationFn(value))) {
+        console.error(errorMessage);
+        throw new Error(errorMessage);
+      }
+      console.log(chalk.greenBright(`✅ ${variable} added.`), chalk.whiteBright("Continuing operation."));
+      overwriteLine(".env", variable, value);
+    } catch (error) {
+      console.error(chalk.redBright(`Failed to set ${variable}.`));
+      throw error;
+    }
+  }
 }
 
 /**
@@ -108,40 +115,40 @@ async function checkEnvVariable(variable: string, errorMessage: string, validati
  * @param newValue - The new value to set for the variable.
  */
 function overwriteLine(filePath: string, variable: string, newValue: string): void {
-    try {
-        const lines = readFileSync(filePath, "utf-8").split("\n");
-        const index = lines.findIndex(line => line.startsWith(variable));
-        if (index !== -1) {
-            lines[index] = `${variable}=${newValue}`;
-        } else {
-            lines.push(`${variable}=${newValue}`);
-        }
-        writeFileSync(filePath, lines.join("\n"));
-    } catch (error) {
-        console.error(chalk.redBright(`Error writing to file ${filePath}: ${error.message}`));
-        throw error;
+  try {
+    const lines = readFileSync(filePath, "utf-8").split("\n");
+    const index = lines.findIndex(line => line.startsWith(variable));
+    if (index !== -1) {
+      lines[index] = `${variable}=${newValue}`;
+    } else {
+      lines.push(`${variable}=${newValue}`);
     }
+    writeFileSync(filePath, lines.join("\n"));
+  } catch (error: unknown) {
+    console.error(chalk.redBright(`Error writing to file ${filePath}.`));
+    throw error;
+  }
 }
 
 /**
  * Creates a .env file with default values if it does not already exist.
  */
-async function createEnvFile(): Promise<void> {
-    if (!existsSync(".env")) {
-        console.log(chalk.yellowBright("⚠ .env file not found."), chalk.whiteBright("Creating one now..."));
-        try {
-            writeFileSync(
-                ".env",
-                `APPLICATION_TOKEN=\n# Your bot token\n\nGUILD_ID=\n# Only an ID here if you want commands to be registered in one server.\n\nAPPLICATION_ID=\n# Your bot's application ID (can be found on Discord or on the Discord Developer Portal)`
-            );
-            console.log(chalk.greenBright("✅ .env file created."));
-        } catch (error) {
-            console.error(chalk.redBright(`Failed to create .env file: ${error.message}`));
-            throw error;
-        }
-    } else {
-        console.log(chalk.yellowBright("✅ .env file already exists."), chalk.whiteBright("Continuing operation...\n"));
+function createEnvFile(): void {
+  if (!existsSync(".env")) {
+    console.log(chalk.yellowBright("⚠ .env file not found."), chalk.whiteBright("Creating one now..."));
+    try {
+      writeFileSync(
+        ".env",
+        "APPLICATION_TOKEN=\n# Your bot token\n\nGUILD_ID=\n# Only put an ID here if you want commands to be registered in one server.\n\nAPPLICATION_ID=\n# Your bot's application ID (can be found on Discord or on the Discord Developer Portal)",
+      );
+      console.log(chalk.greenBright("✅ .env file created."));
+    } catch (error) {
+      console.error(chalk.redBright("Failed to create .env file."));
+      throw error;
     }
+  } else {
+    console.log(chalk.yellowBright("✅ .env file already exists."), chalk.whiteBright("Continuing operation...\n"));
+  }
 }
 
 /**
@@ -149,51 +156,51 @@ async function createEnvFile(): Promise<void> {
  * If they choose per guild, prompts for the GUILD_ID.
  */
 async function askForGuildId(): Promise<void> {
-    try {
-        const response = await promptUser("Do you want to register commands globally? (Y/N) ");
-        if (response.toLowerCase() === "y" || response.toLowerCase() === "yes") {
-            console.log(chalk.greenBright("✅ Commands will be registered globally. No Guild ID required.\n"));
-        } else if (response.toLowerCase() === "n" || response.toLowerCase() === "no") {
-            await checkEnvVariable("GUILD_ID", guildIdError.join("\n"), isSnowflake);
-        } else {
-            console.log(chalk.redBright("Invalid response. Please enter 'Y' or 'N'."));
-            await askForGuildId();
-        }
-    } catch (error) {
-        console.error(chalk.redBright(`Failed to get response for global commands: ${error.message}`));
-        throw error;
+  try {
+    const response = await promptUser("Do you want to register commands globally? (Y/N) ");
+    if (response.toLowerCase() === "y" || response.toLowerCase() === "yes") {
+      console.log(chalk.greenBright("✅ Commands will be registered globally. No Guild ID required.\n"));
+    } else if (response.toLowerCase() === "n" || response.toLowerCase() === "no") {
+      await checkEnvVariable("GUILD_ID", guildIdError.join("\n"), isSnowflake);
+    } else {
+      console.log(chalk.redBright("Invalid response. Please enter 'Y' or 'N'."));
+      await askForGuildId();
     }
+  } catch (error) {
+    console.error(chalk.redBright("Failed to get response for global commands."));
+    throw error;
+  }
 }
 
 /**
  * Main function to check prerequisites.
  * Ensures that necessary environment variables are set and prompts the user if they are not.
  */
-export async function prerequisitesCheck(): Promise<true | void> {
-    console.log(chalk.cyanBright("Starting prerequisites check...\n"));
+export async function prerequisitesCheck(): Promise<void> {
+  console.log(chalk.cyanBright("Starting prerequisites check...\n"));
 
-    try {
-        await createEnvFile();
-    } catch (error) {
-        console.error(chalk.redBright("Failed to create .env file. Exiting..."));
-        process.exit(1);
-    }
+  try {
+    createEnvFile();
+  } catch {
+    console.error(chalk.redBright("Failed to create .env file. Exiting..."));
+    process.exit(1);
+  }
 
-    console.log(envVars.join("\n"));
+  console.log(envVars.join("\n"));
 
-    try {
-        await checkEnvVariable("APPLICATION_TOKEN", tokenError.join("\n"), tokenCheck);
-        await checkEnvVariable("APPLICATION_ID", appIdError.join("\n"), isSnowflake);
-        await askForGuildId();
-    } catch (error) {
-        console.error(chalk.redBright("Prerequisite check failed. Exiting..."));
-        process.exit(1);
-    }
+  try {
+    await checkEnvVariable("APPLICATION_TOKEN", tokenError.join("\n"), tokenCheck);
+    await checkEnvVariable("APPLICATION_ID", appIdError.join("\n"), isSnowflake);
+    await askForGuildId();
+  } catch {
+    console.error(chalk.redBright("Prerequisite check failed. Exiting..."));
+    process.exit(1);
+  }
 
-    console.log(chalk.greenBright("✅ Prerequisite process complete. No issues were found."));
+  console.log(chalk.greenBright("✅ Prerequisite process complete. No issues were found."));
 
-    rl.close();
-    process.exit(0);
+  rl.close();
+  process.exit(0);
 }
 
-prerequisitesCheck();
+await prerequisitesCheck();
