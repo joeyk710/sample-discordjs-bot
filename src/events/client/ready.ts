afa86380c067188899ca09fcb1ddfcf659973d1c
Noah <69383725+peakh@users.noreply.github.com>
2025-06-30 23:30:16 -0400

    Callback does not use asynchronous performances

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -4,7 +4,7 @@ import type { Event } from "../../structures/event.js";
 export default {
   name: Events.ClientReady,
   once: true,
-  async execute(client) {
+  execute(client) {
     console.log(`${client.user.username} is online`);
   },
 } satisfies Event<Events.ClientReady>;
